0339571102f33ce731023fb1a35b67b4ba32bf81
Simone Fiorenza <182414955+asmxes@users.noreply.github.com>
2024-12-25 15:25:06 +0100

    First

---
+++ ry.hpp
@@ -0,0 +1,16 @@
+#ifndef RY_HPP
+#define RY_HPP
+
+typedef signed char s8;
+typedef unsigned char u8;
+typedef signed short s16;
+typedef unsigned short u16;
+typedef signed int s32;
+typedef unsigned int u32;
+typedef signed long s64;
+typedef unsigned long u64;
+typedef float f32;
+typedef double f64;
+typedef u64 ptr;
+
+#endif
